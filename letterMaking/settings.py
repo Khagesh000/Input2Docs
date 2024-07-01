2dac8ff04bb289ec80dfe82e59d5f5518e835361
khagesh000 <kaykarthik9@gmail.com>
2024-07-01 11:37:51 +0530

    Updated settings, views, and URLs

--- letterMaking/settings.py
+++ letterMaking/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = "letterMaking.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR.joinpath('frontend')],
+        "DIRS": [BASE_DIR.joinpath('frontend', 'dist')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
